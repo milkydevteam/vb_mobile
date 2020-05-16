827c9ffae0c69299ada51e05029f4b079e276df0
chienvm2 <chienvm2@fpt.com.vn>
2020-05-16 23:36:26 +0700

    update file name

---
+++ src/themes/Colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  primaryRed: '#ae1c3a',
+  secondRed: '#ffcdd2',
+  white: '#FFF',
+  greenText: '#1b5e20',
+  grey: 'rgba(0,0,0,0.3)',
+  background: '#FAFAFA',
+};
+
+export default colors;
